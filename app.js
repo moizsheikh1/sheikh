8832efd7c2d410bf50bd6eb915da9ff34e9aba5e
moiz <moizsheikh075@gmail.com>
2021-12-09 16:54:49 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,288 @@
+//CHAPTER 01
+//alert("welcome")
+//alert("error! enter a valid password")
+//alert("Welcome to js land\n happy coding")
+//alert("Welcome to js land")
+//alert("HAppy Coding\n Prevent This Page From Creating Additional Dialogs")
+//alert("Hello....I can run Js Through My Web browser cansole")
+//FIRST CHAPTER COMPLETED
+
+
+//CHAPTER 02
+//var username =prompt ("user name")
+//alert (username)
+//var MYNAME =prompt("full name")
+//alert (MYNAME)
+//var message = ("Hello world ")
+//alert(message)
+//var entername =prompt ("enter name")
+//var enterage  =prompt ("enter age")
+//var entersubject = prompt("enter subject")
+//alert(entername)
+//alert(enterage)
+//alert(entersubject)
+//var pizzapoint = ("pizza \n pizz \n piz \n pi \n")
+//alert(pizzapoint)
+//var email =prompt ("Enter Email Address")
+//alert(email)
+//var Book =("A smarter way to learn javascript")
+//alert(Book)
+//var yah = ( "yah! I can write HTML content through javascript"  )
+//document.write(yah)
+//var a =("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+//alert(a)
+//SECOND CHAPTER COMPLETED
+
+
+
+//CHAPTER 03
+//var age =("i am 18year old")
+//alert(age)
+//var birthyear =("My birhth year is 2003")
+//document.write(birthyear)
+//var visitorname  = ("john doe")
+//var producttitel =("xyz clothing store")
+//var quantity = ("T-shirts")
+//document.write(visitorname + producttitel + quantity)
+//CHPATER THREE COMPLETED
+
+
+//CHAPTER 04
+//var name ="john sed",age ="22",username="user571"
+//alert(name + age + username)
+//illeagal
+1 //var 123= ""
+2//var 123abc= ""
+3//var ba$123= ""
+4//var ba'123= ""
+5//var ba!123= ""
+
+//legal
+1//var ba123= ""
+2//var ba$123= ""
+3//var ABC = ""
+4//var a123b = ""
+5//var abc123 =""
+//var a =("A heading stating “Rules for naming JS variables")
+//var b= ("Variable names can only contain ______, ______,______ and ______.")
+//var c=("Variables must begin with a ______, ______ or_____.")
+//var d =("Variable names are case _________")
+//var e =("Variable names should not be JS _______")
+//document.write(a + b + c +d + e)
+//CHAPTER FOUR COMPLETE
+
+
+//CHAPTER 05
+//var a = 3
+//var b= 5
+//var total =a +b 
+//document.write(a+b)
+//var a= 3
+//var b= 5
+//document.write(a-b)
+//var a= 3
+//var b= 5
+//document.write(a*b)
+//var a= 3
+//var b= 5
+//document.write(a/b)
+// var one=("Value  after variable declaration is: undefined ")
+// document.write(one ) 
+// var a =9 
+// var b =a++
+
+
+
+
+// var two=(" Initialvalue: 9")
+// document.write(two)
+// var three=("Value After Increment =10")
+// document.write(three)
+// var c=a+7
+// var four =("Value after addition is: 16")
+// document.write(four)
+// var d=--c
+// var five=("Valueafter decrement is: 15")
+// document.write(five)
+// var e=16/c
+// var six=("The remainder is \=1")
+// document.write(six)
+
+
+//var a =600
+//var b=5
+//var c= ("total cost to buy 5 tickets is")
+//var cost =(c + a*b)
+//document.write(cost)
+
+ //table of 4
+ //var table = 9;
+//var length = 10;
+//var i = 1;
+
+//document.write("Multiplication table: "+ table);
+//for(i = 1; i <= length; i++)
+ //document.write("<br>"+i+" * "+table+" = " +(i * table));
+
+
+//var a = ("price of item first is ")
+//var a1 =650
+//document.write(a+a1)
+//document.write("<br>")
+
+//var b = ("quamtity of 1 item 1 is ")
+//var a2 =3
+//document.write(b + a2)
+//document.write("<br>")
+
+
+//var c = ("price of item 2 is ")
+//var a3 = 100
+//document.write(c +a3)
+//document.write("<br>")
+
+
+//var d = ("quantity of item 2 is ")
+//var a4 = 7
+//document.write(d +a4)
+//document.write("<br>")
+
+
+//var e = ("shipping charges ")
+//var a5 = 100
+//document.write(e + a5)
+//document.write("<br>")
+
+
+//var total = ("a+a1+b+a2+c+a3+d+a4+e+a5")
+//document.write("<br>")
+//document.write(a1+a2+a3+a4+a5)
+//document.write("<br>")
+
+
+
+// var a =+prompt ("marks of english")
+// var b =+prompt ("marks of urdu")
+// var c =+prompt ("marks of maths")
+// var d =+prompt ("marks of chemistry")
+// var e =+prompt ("marks of physics")
+// document.write(a + b + c + d +e)
+
+// // var total=(a+b+c+d+e *100/450)
+// document.write (total)
+
+// var total =(100 + 100 +100+ 100+100)
+// var percentage =(total * 100 / 500)
+// console.log(a+b+c+d+e+total+per)
+
+
+
+
+// var a =(10)
+// var b =(104.80)
+// var c =(25)
+// var d = (28)
+// document.write(a*b+c*d)
+
+
+// var birthYear = 1984;
+// var futureYear  = 2012;
+// var age  = futureYear - birthYear;
+// console.log('I will be either ' + age + ' or ' + (age - 1));
+
+// var radius = 3;
+// var circumference = Math.PI * 2*radius;
+// console.log("The circumference is " + circumference);
+// var area = Math.PI * radius*radius;
+// console.log("The area is " + area);
+
+
+
+
+// var age = 28;
+// var maxAge = 100;
+// var numPerDay = 2;
+// var totalNeeded = (numPerDay * 365) * (maxAge - age);
+// var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+// console.log(message);
+//chapter FIVE COMPLETED
+
+
+
+
+// class GFG
+// {
+ 
+//     static  check(char )
+//     {
+     
+//         if (ch >= 'A' && ch <= 'Z')
+//             System.out.println("\n" + ch +
+//                     " is an UpperCase character");
+     
+//         else if (ch >= 'a' && ch <= 'z')
+//             System.out.println("\n" + ch +
+//                     " is an LowerCase character" );
+     
+//         else
+//             System.out.println("\n" + ch +
+//                     " is not an aplhabetic character" );
+//     }
+// }
+
+
+// var num1, num2;
+// num1 = window.prompt("Input the First integer", "0");
+// num2 = window.prompt("Input the second integer", "0");
+                                                 
+// if(parseInt(num1, 10) > parseInt(num2, 10)) 
+//   { 
+//   console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//   }   
+// else
+//   if(parseInt(num2, 10) > parseInt(num1, 10)) 
+//   {
+//   console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//   }                  
+// else
+//   {
+//    console.log("The values "+ num1+ " and "+num2+ " are equal.");
+//   }
+
+
+
+//  class Demo
+// {
+//      static  main(String) 
+//     {
+//          number=109;
+//         if(number > 0)
+//         {
+//             System.out.println(number+" is a positive number");
+//         }
+//         else if(number < 0)
+//         {
+//             System.out.println(number+" is a negative number");
+//         }
+//         else
+//         {
+//             System.out.println(number+" is neither positive nor negative");
+//         }
+//     }
+// }
+
+
+// function vowelChecker (char) {
+  
+//     if (char === 'a'||'e'||'i'||'o'||'u') {
+//         return "The character is a Vowel";
+//     }
+//     else {
+//     return "The character is a Consonant";
+//     }
+// }
+  
+// console.log (vowelChecker ("e"));
+  
+
+
